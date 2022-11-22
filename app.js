1babcfa12ad9c4efb3ccf6f77293fec324b860c6
기용 <wjdrldyd0213@gmail.com>
2022-11-22 23:57:04 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// 주석
+console.log(234);
+
+const player = {
+    name: "기용",
+    points: 100,
+    alive: true
+};
+
+console.log(player.name);
+
+player.lasName = "정"
+
+// object 전체만 constant
+// 안의 각각 정보는 let으로 저장
+
+console.log(player["name"])
