e131ca2b78c1cd0c5f68a66022a13aded29192e4
기용 <wjdrldyd0213@gmail.com>
2022-11-24 21:08:17 +0900

    challenge done

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 // h1 html element mdn 검색
+// toggle()
 
 const h1 = document.querySelector("h1");
 
  function handleTitleClick(){
-    const clickedClass = "clicked"; // 반복 줄이고 체크 가능해짐.
-
-    if(h1.className === clickedClass){
-        h1.className = "";
-    } else{
-        h1.className = clickedClass;
-    }
-        // CSS를 직접 지정하지 않고 class를 지정한다.
+    h1.classList.toggle("clicked");
  }
 
  h1.addEventListener("click", handleTitleClick);
