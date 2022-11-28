1fa28e2e4f0e4edecb60092c7b22b45bc54ac674
Gi-Yong <wjdrldyd0213@gmail.com>
2022-11-28 11:04:30 +0900

    수정

--- app.js
+++ app.js
@@ -1,29 +1,18 @@
-<<<<<<< HEAD
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 const hello = document.querySelector("h1");
-=======
-// h1 html element mdn 검색
-// toggle()
->>>>>>> e131ca2b78c1cd0c5f68a66022a13aded29192e4
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-<<<<<<< HEAD
 function paintGreetings(){
-    const savedUsername = localStorage.getItem(USERNAME_KEY);
+    const username = localStorage.getItem(USERNAME_KEY);
     hello.innerText = `Hello ${username}`;
     hello.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function onLoginSubmit(event){
     event.preventDefault();
-=======
- function handleTitleClick(){
-    h1.classList.toggle("clicked");
- }
->>>>>>> e131ca2b78c1cd0c5f68a66022a13aded29192e4
 
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
@@ -32,6 +21,8 @@ function onLoginSubmit(event){
     paintGreetings();
 }
 
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
